8c562bbc076f91036de6af85ebf0b14bb7cebd42
root <root@ip-172-31-7-39.ap-northeast-1.compute.internal>
2021-12-31 03:24:12 +0000

    Add autograd example

--- t.py
+++ t.py
@@ -1,9 +1,11 @@
 import torch
+from torch import ops
 import dgl
 from dgl.data import CoraGraphDataset
 torch.set_num_threads(1)
-torch.classes.load_library("build/libTorchDGLGraph.so")
-print(torch.classes.loaded_libraries)
+torch.classes.load_library("libTorchDGLGraph.so")
+torch.classes.load_library("libCustomOp.so")
+# print(torch.classes.loaded_libraries)
 
 g = CoraGraphDataset()[0]
 src, dst = g.edges()
@@ -12,16 +14,24 @@ s = torch.classes.my_classes.TorchDGLGraph(src, dst)
 TorchDGLGraph = torch.classes.my_classes.TorchDGLGraph
 
 
-def do_in_degrees(s: TorchDGLGraph, vids: torch.Tensor):
-    return s.in_degrees(vids);
+def do_in_degrees(s: TorchDGLGraph, vids: torch.Tensor, u: torch.Tensor):
+    return ops.custom.indegree_op_with_autograd(s, vids, u)
+
 
 scripted_func = torch.jit.script(do_in_degrees)
 print(scripted_func.graph)
 print("##########DGL#########")
-print(do_in_degrees(g, torch.tensor([1,3,5,7])))
-print("##########TorchDGL#########")
-print(do_in_degrees(s, torch.tensor([1,3,5,7])))
+u = torch.tensor([1,3,5,7], dtype=torch.float, requires_grad=True)
+output = do_in_degrees(s, torch.tensor([1,3,5,7]), u)
+print(output, u)
+output.sum().backward()
+print(u.grad)
+
 print("##########TorchScript DGL#########")
-print(scripted_func(s, torch.tensor([1,3,5,7])))
+u = torch.tensor([1,3,5,7], dtype=torch.float, requires_grad=True)
+output = do_in_degrees(s, torch.tensor([1,3,5,7]), u)
+print(output, u)
+output.sum().backward()
+print(u.grad)
 
 scripted_func.save("in_degree.pt")
